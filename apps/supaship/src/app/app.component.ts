89d24a5903569ab186c9a0a7f1f291ca86d2247b
Zack DeRose <zack.derose@gmail.com>
2022-11-29 19:33:23 -0700

    Initial commit

---
+++ apps/supaship/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-supaship-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'supaship';
+}
